a97dd8c301510a715ebb0696e9094cab3c5de552
Darren Kidd <dev@kidd.life>
2020-06-12 03:42:21 +0000

    Use goroutine and WaitGroup
    This is a bit scary... the memoisation still seems
    to be happening which means each of the goroutines
    is accessing my (non-threadsafe) array?

--- README.md
+++ README.md
@@ -33,7 +33,7 @@ user    2m19.272s
 sys     0m0.510s
 ```
 
-### latest
+### `a194f9a`
 
 ```bash
 $ go build fib.go && time ./fib 42 51 19 27 30
@@ -46,6 +46,21 @@ $ go build fib.go && time ./fib 42 51 19 27 30
 real    0m0.029s
 user    0m0.002s
 sys     0m0.004s
+```
+
+### latest
+
+```bash
+$ go build fib.go && time ./fib 42 51 19 27 30
+[2] fib(51) --> 32951280099
+[3] fib(19) --> 6765
+[4] fib(27) --> 317811
+[5] fib(30) --> 1346269
+[1] fib(42) --> 433494437
+
+real    0m0.371s
+user    0m0.004s
+sys     0m0.002s
 ```
 
 ## Old Way
--- src/fib.go
+++ src/fib.go
@@ -5,9 +5,11 @@ import (
 	"fmt"
 	"os"
 	"strconv"
+	"sync"
 )
 
-func fib(pos int, num string) {
+func fib(pos int, num string, wg *sync.WaitGroup) {
+	defer wg.Done()
 	if n, err := strconv.ParseInt(num, 10, 0); err == nil {
 		fibOfN := fibonacci.CalcFib(n)
 		fmt.Printf("[%d] fib(%d) --> %d\n", pos, n, fibOfN)
@@ -15,8 +17,11 @@ func fib(pos int, num string) {
 }
 
 func main() {
+	var wg sync.WaitGroup
 	seq := os.Args[1:]
 	for i := 0; i < len(seq); i++ {
-		fib(i+1, seq[i])
+		wg.Add(1)
+		go fib(i+1, seq[i], &wg)
 	}
+	wg.Wait()
 }
