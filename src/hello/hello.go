2e518532d0c3f023b35101f49b6d962fd6af8de4
Darren Kidd <dev@kidd.life>
2020-06-12 00:14:14 +0000

    Followed original README to try some things out
    Have now cleared README and will delete the hello module soon.

--- src/hello/hello.go
+++ src/hello/hello.go
@@ -1,24 +1,103 @@
 package hello
 
-import ()
+import "net/http"
 
 // User user type
 type User struct {
-	ID   int64
-	Name string
-	Addr *Address
+	ID   int64    `json:"id,omitempty"`
+	Name string   `json:"name,omitempty"`
+	Addr *Address `json:"addr,omitempty"`
 }
 
 // Address address type
 type Address struct {
-	City   string
-	ZIP    int
-	LatLng [2]float64
+	City   string     `json:"city,omitempty"`
+	ZIP    int        `json:"zip,omitempty"`
+	LatLng [2]float64 `json:"lat_lng,omitempty"`
 }
 
-var alex = User{}
+type mock struct{}
+
+// Header returns the header map that will be sent by
+// WriteHeader. The Header map also is the mechanism with which
+// Handlers can set HTTP trailers.
+//
+// Changing the header map after a call to WriteHeader (or
+// Write) has no effect unless the modified headers are
+// trailers.
+//
+// There are two ways to set Trailers. The preferred way is to
+// predeclare in the headers which trailers you will later
+// send by setting the "Trailer" header to the names of the
+// trailer keys which will come later. In this case, those
+// keys of the Header map are treated as if they were
+// trailers. See the example. The second way, for trailer
+// keys not known to the Handler until after the first Write,
+// is to prefix the Header map keys with the TrailerPrefix
+// constant value. See TrailerPrefix.
+//
+// To suppress automatic response headers (such as "Date"), set
+// their value to nil.
+func (m *mock) Header() http.Header {
+	panic("not implemented") // TODO: Implement
+}
+
+// Write writes the data to the connection as part of an HTTP reply.
+//
+// If WriteHeader has not yet been called, Write calls
+// WriteHeader(http.StatusOK) before writing the data. If the Header
+// does not contain a Content-Type line, Write adds a Content-Type set
+// to the result of passing the initial 512 bytes of written data to
+// DetectContentType. Additionally, if the total size of all written
+// data is under a few KB and there are no Flush calls, the
+// Content-Length header is added automatically.
+//
+// Depending on the HTTP protocol version and the client, calling
+// Write or WriteHeader may prevent future reads on the
+// Request.Body. For HTTP/1.x requests, handlers should read any
+// needed request body data before writing the response. Once the
+// headers have been flushed (due to either an explicit Flusher.Flush
+// call or writing enough data to trigger a flush), the request body
+// may be unavailable. For HTTP/2 requests, the Go HTTP server permits
+// handlers to continue to read the request body while concurrently
+// writing the response. However, such behavior may not be supported
+// by all HTTP/2 clients. Handlers should read before writing if
+// possible to maximize compatibility.
+func (m *mock) Write(_ []byte) (int, error) {
+	panic("not implemented") // TODO: Implement
+}
+
+// WriteHeader sends an HTTP response header with the provided
+// status code.
+//
+// If WriteHeader is not called explicitly, the first call to Write
+// will trigger an implicit WriteHeader(http.StatusOK).
+// Thus explicit calls to WriteHeader are mainly used to
+// send error codes.
+//
+// The provided code must be a valid HTTP 1xx-5xx status code.
+// Only one header may be written. Go does not currently
+// support sending user-defined 1xx informational headers,
+// with the exception of 100-continue response header that the
+// Server sends automatically when the Request.Body is read.
+func (m *mock) WriteHeader(statusCode int) {
+	panic("not implemented") // TODO: Implement
+}
+
+var darren = User{
+	ID:   1,
+	Name: "Darren Kidd",
+	Addr: &Address{
+		City: "Wellington",
+		ZIP:  6011,
+		LatLng: [2]float64{
+			0.0,
+			0.0,
+		},
+	},
+}
 
 // Hello writes a welcome string
 func Hello() string {
-	return "Hello, " + alex.Name
+	return "Hello, " + darren.Name
 }
